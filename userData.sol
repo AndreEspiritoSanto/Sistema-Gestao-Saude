60e4106cad8e6fe0474c2f45d09cbe0fcb5cfae0
AndreEspiritoSanto <165073433+AndreEspiritoSanto@users.noreply.github.com>
2024-04-07 11:42:19 +0100

    Correção de comentários

--- userData.sol
+++ userData.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract UserData {
-  // Define a struct to hold user information
+  
+  //Definir a estrutura de dados para armazenar a informação
   struct User {
     uint256 userId;
     string name;
@@ -22,18 +23,18 @@ contract UserData {
       //string userType;
   }
 
-  // Mapping to store user data with their ID as the key
+  // Mapear para armazenar a informação mapeada para o ID como chave
   mapping(uint256 => User) public users;
 
-  // Counter to keep track of assigned IDs
+  // Contador para guardar registo dos IDs
   uint256 private nextUserId = 1;
 
-  // Function to add a new user and assign an ID
+  // Função para arquivar informação e assignar um ID
   function addUser(uint256 _userId, string memory _name, uint256 _nationalID, uint256 _socialsecurityID, string memory _contact, string memory _Address, string memory _DateofBirth, string memory _extraParameters ) public {
     users[_userId] = User(_userId, _name, _nationalID, _socialsecurityID, _contact, _Address, _DateofBirth, _extraParameters);
   }
 
-  // Function to retrieve a user's information by ID
+  // Função para recuperar informação pelo ID
   function getUser(uint256 _userId) public view returns (User memory) {
     return users[_userId];
   }
